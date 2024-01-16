224cdae78439cc2c2f7e2668b9db82054dbb5f20
Priyanjith N <jithpriyan2006@gmail.com>
2024-01-16 10:13:42 +0530

    admin routes code std and all axios removed from backend 16-01-2024T10:13:00:00:00

--- server/router/adminSide/adminRouter.js
+++ server/router/adminSide/adminRouter.js
@@ -7,67 +7,114 @@ const adminAuthMiddleware = require('../../../middleware/adminSide/authMiddlewar
 const store = require('../../controller/adminSide/multer');
 const adminBannerController = require('../../controller/adminSide/adminBannerController');
 
-router.get('/adminLogin', adminAuthMiddleware.noAdminAuth, adminRender.adminLogin);
 
-router.post('/adminLogin', adminController.adminLogin);
 
-router.get('/adminHome' , adminAuthMiddleware.isAdminAuth, adminRender.adminHome);
+// Admin Login Routes
+router.route('/adminLogin')
+    .get(
+        adminAuthMiddleware.noAdminAuth,
+        adminRender.adminLogin
+    )
+    .post(
+        adminAuthMiddleware.noAdminAuth,
+        adminController.adminLogin
+    );
 
-router.get('/adminAddProduct', adminAuthMiddleware.isAdminAuth, adminRender.adminAddProducts);
+    
+    
+// Admin Dashboard Route
+router.get('/adminHome' , adminAuthMiddleware.isAdminAuth, adminRender.adminHome); // get the page of dashboard
 
-router.post('/adminAddProduct', adminAuthMiddleware.isAdminAuth, store.array('image', 4), adminController.adminAddProduct);
+router.get('/downloadSalesReport', adminAuthMiddleware.isAdminAuth, adminController.downloadSalesReport); // Option to download sales report
 
-router.get('/adminProductManagement',adminAuthMiddleware.isAdminAuth, adminRender.adminProductManagement);
 
-router.get('/adminUnlistedProduct', adminAuthMiddleware.isAdminAuth, adminRender.adminUnlistedProduct);
 
-router.get('/adminCategoryManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminCategoryManagement);
+// Admin Product Management Routes
+router.get('/adminProductManagement',adminAuthMiddleware.isAdminAuth, adminRender.adminProductManagement); // Product is listed in Page to manage
 
-router.get('/adminAddCategory', adminAuthMiddleware.isAdminAuth, adminRender.adminAddCategory);
+router.get('/adminUnlistedProduct', adminAuthMiddleware.isAdminAuth, adminRender.adminUnlistedProduct); // To manage unlisted product
 
-router.post('/adminAddCategory', adminAuthMiddleware.isAdminAuth, adminController.adminAddCategory);
+router.route('/adminAddProduct')
+    .get(
+        adminAuthMiddleware.isAdminAuth,
+        adminRender.adminAddProducts
+    )
+    .post(
+        adminAuthMiddleware.isAdminAuth,
+        store.array('image', 4),
+        adminController.adminAddProduct
+    );
 
-router.get('/adminUnlistedCategory', adminAuthMiddleware.isAdminAuth, adminRender.adminUnlistedCategory);
+router.get('/adminSoftDeleteProduct/:id', adminAuthMiddleware.isAdminAuth, adminController.adminSoftDeleteProduct);// Option to make product unlisted 
 
-router.get('/adminSoftDeleteCategory/:id', adminAuthMiddleware.isAdminAuth, adminController.adminSoftDeleteCategory);
+router.get('/adminRestoreProduct/:id',adminAuthMiddleware.isAdminAuth, adminController.adminRestoreProduct);// Option to make product listed
 
-router.get('/adminRestoreCategory/:id', adminAuthMiddleware.isAdminAuth, adminController.adminRestoreCategory);
+router.get('/adminUpdateProduct/:id', adminAuthMiddleware.isAdminAuth, adminRender.adminUpdateProduct); // Update product page
 
-router.get('/adminSoftDeleteProduct/:id', adminAuthMiddleware.isAdminAuth, adminController.adminSoftDeleteProduct);
+router.get('/adminDeleteProductImg', adminAuthMiddleware.isAdminAuth, adminController.adminDeleteProductImg);// Option to delete img of product without unlink it's soft delete
 
-router.get('/adminRestoreProduct/:id',adminAuthMiddleware.isAdminAuth, adminController.adminRestoreProduct);
+router.post('/adminUpdateProduct', adminAuthMiddleware.isAdminAuth, store.array('fileInput', 4), adminController.adminUpdateProduct);// Updates the product
 
-router.get('/adminUpdateProduct/:id', adminAuthMiddleware.isAdminAuth, adminRender.adminUpdateProduct);
 
-router.get('/adminDeleteProductImg', adminAuthMiddleware.isAdminAuth, adminController.adminDeleteProductImg);
 
-router.post('/adminUpdateProduct', adminAuthMiddleware.isAdminAuth, store.array('fileInput', 4), adminController.adminUpdateProduct);
+// Admin Category Management Routes
+router.get('/adminCategoryManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminCategoryManagement); // all listed category can be managed
 
-router.get('/adminUserManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminUserManagement);
+router.get('/adminUnlistedCategory', adminAuthMiddleware.isAdminAuth, adminRender.adminUnlistedCategory);// all unlisted category can be manage
 
-router.get('/adminUserStaus/:id/:block', adminAuthMiddleware.isAdminAuth, adminController.adminUserStatus);
+router.route('/adminAddCategory')
+    .get(
+        adminAuthMiddleware.isAdminAuth,
+        adminRender.adminAddCategory
+    )
+    .post(
+        adminAuthMiddleware.isAdminAuth,
+        adminController.adminAddCategory
+    );
 
-router.get('/adminUserDelete/:id', adminAuthMiddleware.isAdminAuth, adminController.adminUserDelete);
+router.get('/adminSoftDeleteCategory/:id', adminAuthMiddleware.isAdminAuth, adminController.adminSoftDeleteCategory);// Option to make category unlisted
 
-router.get('/adminOrderManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminOrderManagement); 
+router.get('/adminRestoreCategory/:id', adminAuthMiddleware.isAdminAuth, adminController.adminRestoreCategory);// Option to make category listed
 
-router.post('/adminChangeOrderStatus/:orderId/:productId', adminAuthMiddleware.isAdminAuth, adminController.adminChangeOrderStatus); 
 
-router.get('/downloadSalesReport', adminAuthMiddleware.isAdminAuth, adminController.downloadSalesReport); 
 
-router.get('/adminBannerManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminBannerManagement);
+// Admin User Management Routes
+router.get('/adminUserManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminUserManagement);// Here all listed users can be managed
 
-router.get('/adminAddBanner', adminAuthMiddleware.isAdminAuth, adminRender.adminAddBanner);
+router.get('/adminUserStaus/:id/:block', adminAuthMiddleware.isAdminAuth, adminController.adminUserStatus);// Option to block and unblock user
 
-router.post('/adminAddBanner', adminAuthMiddleware.isAdminAuth, store.fields([{name: 'largeImg', maxCount: 1}, {name: 'smallImg', maxCount: 1}]), adminBannerController.adminAddBanner); 
+router.get('/adminUserDelete/:id', adminAuthMiddleware.isAdminAuth, adminController.adminUserDelete);// Option to delete users permanently
 
-router.patch('/adminDeleteBanner/:bannerId', adminAuthMiddleware.isAdminAuth,  adminBannerController.adminDeleteBanner);
 
-router.get('/adminUnlistedBanner', adminAuthMiddleware.isAdminAuth,  adminRender.adminUnlistedBannerManagement);
 
-router.patch('/adminRestoreBanner/:bannerId', adminAuthMiddleware.isAdminAuth, adminBannerController.adminRestoreBanner);
+// Admin Order Management Routes
+router.get('/adminOrderManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminOrderManagement); // All order are listed and can be managed
 
-router.delete('/adminPremenentDeleteBanner/:bannerId',  adminAuthMiddleware.isAdminAuth, adminBannerController.adminPremenentDeleteBanner);
+router.post('/adminChangeOrderStatus/:orderId/:productId', adminAuthMiddleware.isAdminAuth, adminController.adminChangeOrderStatus); // Option to change order status
+
+
+
+// Admin Banner Management Routes
+router.get('/adminBannerManagement', adminAuthMiddleware.isAdminAuth, adminRender.adminBannerManagement);// All listed banner can be managed here
+
+router.get('/adminUnlistedBanner', adminAuthMiddleware.isAdminAuth,  adminRender.adminUnlistedBannerManagement); // All unlisted banner can be managed her
+
+router.route('/adminAddBanner')
+        .get(
+            adminAuthMiddleware.isAdminAuth,
+            adminRender.adminAddBanner
+        )
+        .post(
+            adminAuthMiddleware.isAdminAuth,
+            store.fields([{name: 'largeImg', maxCount: 1}, {name: 'smallImg', maxCount: 1}]),
+            adminBannerController.adminAddBanner
+        );
+
+router.patch('/adminDeleteBanner/:bannerId', adminAuthMiddleware.isAdminAuth,  adminBannerController.adminDeleteBanner);// Option to make banner unlisted
+
+router.patch('/adminRestoreBanner/:bannerId', adminAuthMiddleware.isAdminAuth, adminBannerController.adminRestoreBanner);// Option to make banner listed
+
+router.delete('/adminPremenentDeleteBanner/:bannerId',  adminAuthMiddleware.isAdminAuth, adminBannerController.adminPremenentDeleteBanner);// Option to delete banner permanently
 
 
 
@@ -100,7 +147,7 @@ router.get('/adminLogout', adminAuthMiddleware.isAdminAuth, adminController.admi
 
 
 //api
-router.post('/api/getDetailsChart', adminAuthMiddleware.isAdminAuth, adminController.getDetailsChart);
+router.post('/api/getDetailsChart', adminAuthMiddleware.isAdminAuth, adminController.getDetailsChart);// Option to get chart details
  
 
 
