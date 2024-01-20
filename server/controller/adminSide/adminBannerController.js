e4bbf8911daeb6a1dab7c5973fcb14a5d6fcdac8
Priyanjith N <jithpriyan2006@gmail.com>
2024-01-20 14:26:00 +0530

    image crop added for banner in admin side

--- server/controller/adminSide/adminBannerController.js
+++ server/controller/adminSide/adminBannerController.js
@@ -1,4 +1,7 @@
 const adminHelper = require("../../databaseHelpers/adminHelper");
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     adminAddBanner: async (req, res) => {
@@ -25,12 +28,37 @@ module.exports = {
     
             if(req.session.category || req.session.bDescription || req.session.bName){
                 req.session.savedInfo = req.body;
+                //if large img uploaded it is stored localy so delete it
+                if(req.files?.largeImg && req.files?.largeImg?.length !== 0){
+                    fs.unlinkSync(path.join(__dirname, '../../../public',`/uploadedImages/${req.files.largeImg[0].filename}`));
+                }
+                //if small img uploaded it is stored localy so delete it
+                if(req.files?.smallImg && req.files.smallImg.length !== 0){
+                fs.unlinkSync(path.join(__dirname, '../../../public',`/uploadedImages/${req.files.smallImg[0].filename}`));
+                }
+
                 return res.status(401).redirect('/adminAddBanner');
             }
+
+            // crop largeImg
+            await sharp(path.join(__dirname, '../../../public',`/uploadedImages/${req.files.largeImg[0].filename}`))
+            .resize(2000, 888, {
+                fit: "cover",
+                position: "centre",
+            })
+            .toFile(path.join(__dirname, '../../../public',`/uploadedImages/resizedImg${req.files.largeImg[0].filename}`));
+        
+        
+            //crop smallImg
+            await sharp(path.join(__dirname, '../../../public',`/uploadedImages/${req.files.smallImg[0].filename}`))
+            .resize(822, 1200, {
+                fit: "cover",
+                position: "centre",
+            })
+            .toFile(path.join(__dirname, '../../../public',`/uploadedImages/resizedImg${req.files.smallImg[0].filename}`));
     
-            console.log(req.body);
-            const largeImg = `/uploadedImages/${req.files.largeImg[0].filename}`;
-            const smallImg = `/uploadedImages/${req.files.smallImg[0].filename}`;
+            const largeImg = `/uploadedImages/resizedImg${req.files.largeImg[0].filename}`;
+            const smallImg = `/uploadedImages/resizedImg${req.files.smallImg[0].filename}`;
 
             await adminHelper.addBanner(req.body, largeImg, smallImg);
 
